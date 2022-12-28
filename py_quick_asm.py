1b287276ce87daa9638fc5e3ce4271924363ab23
RobertJN64 <robertjnies@gmail.com>
2022-12-28 13:37:59 -0500

    fix vscode theme

--- py_quick_asm.py
+++ py_quick_asm.py
@@ -2,10 +2,10 @@ import os
 
 def main():
     fname = input("Enter filename: ")
-    with open("Assembly Programs/" + fname + ".s"):
+    with open("Assembly Programs/" + fname + ".65c02.s"):
         pass
 
     # http://www.compilers.de/vasm.html
-    os.system(f'cd "Assembly Programs" && vasm6502_oldstyle {fname}.s -Fbin -dotdir -o {fname}.bin')
+    os.system(f'cd "Assembly Programs" && vasm6502_oldstyle {fname}.65c02.s -Fbin -dotdir -o {fname}.bin')
 
 main()
