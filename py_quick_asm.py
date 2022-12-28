ca7790649b9a8be3eb42b72ca1fe697dcbc8a1ab
RobertJN64 <robertjnies@gmail.com>
2022-12-28 00:43:28 -0500

    url

--- py_quick_asm.py
+++ py_quick_asm.py
@@ -5,6 +5,7 @@ def main():
     with open("Assembly Programs/" + fname + ".s"):
         pass
 
+    # http://www.compilers.de/vasm.html
     os.system(f'cd "Assembly Programs" && vasm6502_oldstyle {fname}.s -Fbin -dotdir -o {fname}.bin')
 
 main()
