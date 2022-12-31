552269cf7205053e8acadf7d7c7bfa4ec7339071
RobertJN64 <robertjnies@gmail.com>
2022-12-30 23:33:05 -0500

    assemble to eeprom

--- py_quick_asm.py
+++ py_quick_asm.py
@@ -7,4 +7,8 @@ def main():
     # http://www.compilers.de/vasm.html
     os.system(f'cd VASM && vasm6502_oldstyle.exe "Assembly Programs/{fname}.65c02.s" -Fbin -dotdir -o bin/{fname}.bin')
 
+    if input("Prepare to write EEPROM? y/n ") == "y":
+        with open("VASM/bin/" + fname + ".bin", "rb") as f:
+            with open("rom.bin", "wb") as g:
+                g.write(f.read())
 main()
