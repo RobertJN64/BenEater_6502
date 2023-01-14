e36283dd95bbe893e03a212447aaaf2aee6ead34
RobertJN64 <robertjnies@gmail.com>
2023-01-14 15:53:19 -0500

    removed unneed link

--- VASM/MacroASM/bin_dec.65c02.s
+++ VASM/MacroASM/bin_dec.65c02.s
@@ -1,11 +1,5 @@
   .org $8000
 
-include("lcd")
- ; START HEADER
-lcd_command, lcd_char, lcd_wait, lcd_init
-LCD_CONTROL, LCD_DATA, DDR_LCD_CONTROL, DDR_LCD_DATA, RS, RW, E, BF
- ; END HEADER
-
 include("print_num")
  ; START HEADER
 print_number
